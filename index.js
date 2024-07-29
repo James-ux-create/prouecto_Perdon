a5d80eea51cfd62ff35d88328a8020c2c79a6618
James-ux-create <yalijames25@gmail.com>
2024-07-28 23:08:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que me ibas a perdonar')
+    alert('Sabia que me ibas a disculpar, jijiji')
 });
 
 const noBtn = document.querySelector('#noBtn');
