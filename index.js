cede5c4548138ece45ba1303555e2ab713182d05
James-ux-create <yalijames25@gmail.com>
2024-08-12 13:05:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Mi cerebro se quema haciendo esto xD, pero vale la pena')
+    alert('Mi cerebro se quema haciendo esto, pero vale la pena')
 });
 
 const noBtn = document.querySelector('#noBtn');
